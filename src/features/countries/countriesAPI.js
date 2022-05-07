eb7d2b1ae09535dfd4a3bb6c7a4a4a4b324be15c
Moises <mhdezcoronado@gmail.com>
2022-05-07 23:26:44 +0200

    Fix search country end-point

--- src/features/countries/countriesAPI.js
+++ src/features/countries/countriesAPI.js
@@ -1,11 +1,11 @@
 import axios from 'axios';
 
 const ORIGINAL_URL = 'https://restcountries.com/v2/all';
-const BASE_URL = 'https://restcountries-app.herokuapp.com/api/v1/countries';
+const BASE_URL = 'https://restcountries-app.herokuapp.com/api/v1/countries/search-all?query=';
 const TOKEN = JSON.parse(localStorage.getItem('token'));
 
 export const fetchCountries = (name = '') => axios
-  .get(`${BASE_URL}/search-all?query=${name}`, { headers: { Authorization: TOKEN } })
+  .get(`${BASE_URL}${name}`, { headers: { Authorization: TOKEN } })
   .then((response) => response.data);
 
 export const fetchAllCountries = () => axios
